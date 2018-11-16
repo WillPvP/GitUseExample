872530b9cdda2402f8b712176c940ad758cef307
Hunter Williams <147801@s020rmc148-12.local>
2018-11-16 13:47:37 -0600

    Add new map for literacy feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Williams, Hunter L on 11/16/18.
+//  Copyright © 2018 Williams, Hunter L. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
